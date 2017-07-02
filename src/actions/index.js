4c18e29de4ba8119ab55f672b1fc35e1595bba14
Po-Yu Chen <p820316@gmail.com>
2017-07-02 00:07:13 -0700

    Fixed state related bug by a clone function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,21 @@
+import utils from '../utils'
+
 export default {
   game: {
     init: ({cells}) => ({cells: Array(50).fill(Array(50).fill(0))})
   },
   cell: {
-    toggle: ({cells} , _, { id: {row, col} } ) => {
-      cells[row][col] = cells[row][col] === 1 ? 0 : 1
-      return { cells }
+    toggle: (state , _, {row, col} ) => {
+      let newState = utils.clone(state)
+      newState.cells[row][col] = newState.cells[row][col] ? 0 : 1
+      return newState
     },
     set: ({ cells }, _, { id, val }) => {
       cells[id] = val
       return { cells }
     },
+  },
+  debug: {
+    logState: (state) => console.log('STATE:', state)
   }
 };
--- src/views/index.js
+++ src/views/index.js
@@ -2,10 +2,11 @@ import { h } from 'hyperapp'
 import Board from './board'
 import Controls from './controls'
 
-export default (state, { game, cell }) => (
+export default (state, { game, cell, debug}) => (
   <div>
     <h1>Game of Life</h1>
     <Controls actions={game} />
     <Board cells={state.cells} toggleCell={cell.toggle} />
+    <button onclick={() => debug.logState()}>Debug</button>
   </div>
 )
